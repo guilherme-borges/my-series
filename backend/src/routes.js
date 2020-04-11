50df728dae3813a79115360bedec956b8c078480
Guilherme Borges <guilherme.borges.dev@gmail.com>
2020-04-10 23:59:29 -0300

    Adicionado funcionalidade de gerar token jwt ao logar

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = express.Router();
+const authMiddleware = require('./middlewares/auth');
 
 const UserController = require('./controllers/UserController');
 const SerieController = require('./controllers/SerieController');
@@ -7,6 +8,7 @@ const SessionController = require('./controllers/SessionController');
 
 routes.post('/sessions', SessionController.create);
 
+routes.use(authMiddleware);
 routes.post('/users', UserController.create);
 
 routes.post('/series', SerieController.create);
